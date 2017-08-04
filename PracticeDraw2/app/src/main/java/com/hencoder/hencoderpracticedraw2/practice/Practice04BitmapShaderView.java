e4368cb63ba254f1921a6862cf4bfc4904d9eea9
freshxu <freshxu110@163.com>
2017-08-04 18:34:06 +0800

    Init

--- PracticeDraw2/app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice04BitmapShaderView.java
+++ PracticeDraw2/app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice04BitmapShaderView.java
@@ -1,11 +1,16 @@
 package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.hencoder.hencoderpracticedraw2.R;
 
 public class Practice04BitmapShaderView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -25,6 +30,7 @@ public class Practice04BitmapShaderView extends View {
     {
         // 用 Paint.setShader(shader) 设置一个 BitmapShader
         // Bitmap: R.drawable.batman
+        paint.setShader(new BitmapShader(BitmapFactory.decodeResource(getResources(), R.drawable.batman), Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
     }
 
     @Override
--- PracticeDraw2/app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice05ComposeShaderView.java
+++ PracticeDraw2/app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice05ComposeShaderView.java
@@ -1,11 +1,18 @@
 package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.ComposeShader;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.hencoder.hencoderpracticedraw2.R;
 
 public class Practice05ComposeShaderView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -28,6 +35,11 @@ public class Practice05ComposeShaderView extends View {
         // 用 Paint.setShader(shader) 设置一个 ComposeShader
         // Shader 1: BitmapShader 图片：R.drawable.batman
         // Shader 2: BitmapShader 图片：R.drawable.batman_logo
+
+        Shader shader1 = new BitmapShader(BitmapFactory.decodeResource(getResources(), R.drawable.batman), Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        Shader shader2 = new BitmapShader(BitmapFactory.decodeResource(getResources(), R.drawable.batman_logo), Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        ComposeShader composeShader = new ComposeShader(shader1, shader2, PorterDuff.Mode.DST_IN);
+        paint.setShader(composeShader);
     }
 
     @Override
